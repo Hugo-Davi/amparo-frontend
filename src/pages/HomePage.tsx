7b99d47015165bcbda23543065af107bacf7bf8e
Hugo-Davi <hugodavisilvadeandrade@gmail.com>
2024-11-09 14:39:28 -0300

    Commit inicial AMPARO FRONT END

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <div>
+            <div className="text-4xl">
+                Home page
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
